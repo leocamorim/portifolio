af054e6ec07f1197ef758541aa6e03b58a805099
leocamorim <36824815+leocamorim@users.noreply.github.com>
2022-06-23 13:01:46 -0300

    Popletter animation fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+let $: any;
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,18 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let aux = document.getElementsByClassName("spanPop");
+    for (let i = 0; i < aux.length; i++) {
+      const span = aux[i];
+      span.addEventListener("mouseover", this.mOver, false);
+    }
+  }
+
+  mOver(event: any) {
+    const element = event.srcElement;
+    element.classList.add("animating");
+    setTimeout(() => {
+      element.classList.remove("animating");
+    }, 1000);
   }
 }
