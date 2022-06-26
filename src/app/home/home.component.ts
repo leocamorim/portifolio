71b7b9c5fa4b3d3d591f1819b6af9034e7c4a900
leocamorim <36824815+leocamorim@users.noreply.github.com>
2022-06-26 16:44:23 -0300

    Animation Controlled by JS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,84 @@ let $: any;
 })
 export class HomeComponent implements OnInit {
 
+  initAnimationDelays = [
+    {
+      name: "Header Logo",
+      selector: ".logo",
+      delay: 0.5
+    },
+    {
+      name: "Header - Button Resume",
+      selector: "header button.bt-main",
+      delay: 0.2
+    },
+    {
+      name: "Header - Contact",
+      selector: "header .contact",
+      delay: 0.2
+    },
+    {
+      name: "Header - Experience",
+      selector: "header .experience",
+      delay: 0.2
+    },
+    {
+      name: "Header - About",
+      selector: "header .about",
+      delay: 0.2
+    },
+    {
+      name: "Section 1 - Salutation",
+      selector: "#page1 .salutation",
+      delay: 0.3
+    },
+    {
+      name: "Section 1 - H1",
+      selector: "#page1 h1",
+      delay: 0.2
+    },
+    {
+      name: "Section 1 - Text",
+      selector: "#page1 p",
+      delay: 0.2
+    },
+    {
+      name: "Section 1 - Button",
+      selector: "#page1 button",
+      delay: 0.2
+    },
+    {
+      name: "Left Sidebar - Vertical Bar",
+      selector: "#sidebar-left .vertical-bar",
+      delay: 0.2
+    },
+    {
+      name: "Left Sidebar - Curriculum",
+      selector: "#sidebar-left .curriculum",
+      delay: 0.5
+    },
+    {
+      name: "Left Sidebar - Github",
+      selector: "#sidebar-left .github",
+      delay: 0.2
+    },
+    {
+      name: "Left Sidebar - LinkedIn",
+      selector: "#sidebar-left .linkedin",
+      delay: 0.2
+    },
+    {
+      name: "Right Sidebar - Vertical Bar",
+      selector: "#sidebar-right .vertical-bar",
+      delay: 0.2
+    },
+    {
+      name: "Right Sidebar - Email",
+      selector: "#sidebar-right a",
+      delay: 0.5
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
@@ -16,6 +94,14 @@ export class HomeComponent implements OnInit {
       const span = aux[i];
       span.addEventListener("mouseover", this.mOver, false);
     }
+
+    let currentDelay = 0;
+
+    this.initAnimationDelays.forEach(animation => {
+      currentDelay += animation.delay;
+      let elemento = document.querySelector(animation.selector);
+      elemento?.setAttribute("style", "animation-delay: " + currentDelay + "s")
+    });
   }
 
   mOver(event: any) {
